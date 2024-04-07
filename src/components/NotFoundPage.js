bc0270908d1299caccebb61614e9d6cb7104ed35
Diyaridem <netdiy32@hotmail.com>
2024-04-08 00:04:18 +0300

    the project is finished

--- src/components/NotFoundPage.js
+++ src/components/NotFoundPage.js
@@ -1,11 +1,20 @@
-import React from 'react'
+import React from 'react';
 
 const NotFoundPage = () => {
-    return (
-        <div>
-            404 Page
+  return (
+    <section className="section">
+      <div className="container">
+        <div className="columns is-centered">
+          <div className="column is-half">
+            <div className="notification is-danger has-text-centered">
+              <h1 className="title">404 Page Not Found</h1>
+              <p className="subtitle">Oops! The page you are looking for does not exist.</p>
+            </div>
+          </div>
         </div>
-    )
-}
+      </div>
+    </section>
+  );
+};
 
-export default NotFoundPage
+export default NotFoundPage;
